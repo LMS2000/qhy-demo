76c03d56636e3a3585bf38b2c89a03fe5db140b6
LMS <956287248@qq.com>
2023-04-25 09:44:41 +0800

    qhy-cover demo

---
+++ qhy-auth/src/test/java/com/auth/AuthTest.java
@@ -0,0 +1,19 @@
+package com.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest(classes = AuthApplication.class)
+public class AuthTest {
+
+
+    @Resource
+
+    @Test
+    public void tset(){
+
+    }
+
+}
---
+++ qhy-course-server/src/test/java/com/course/CourseTewsrt.java
@@ -0,0 +1,8 @@
+package com.course;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringBootTest(classes = CourseServerApplication.class)
+public class CourseTewsrt {
+}
